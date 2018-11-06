e171b973df89dc42875b071c56be65311c4f9d70
Rhian Evans <rhian.evans100@btinternet.com>
2018-11-06 19:09:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-/* Enter the code to remove the main node element under this comment */
+document.createElement(div)
+const element'item' =document.body.querySelector
+
+element=<div>'item'</div>
+remove the main node element under this comment */
 
 /* Create your new element here and assign it to newHeader */
 const newHeader = null;
+var element = document.createElement('h1')
